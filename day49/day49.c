96479ec55e80e88ee8f9c6716d4f64d32ccedac0
aanyaa259 <aanyaverma259@gmail.com>
2025-11-09 12:19:03 +0530

    cmmit45

---
+++ day49/day49.c
@@ -0,0 +1,31 @@
+/* Q97: Print the initials of a name.
+
+Sample Test Cases:
+Input 1:
+John Doe
+Output 1:
+J.D.
+
+*/
+#include <stdio.h>
+
+int main() {
+    char name[100];
+    int i;
+
+    printf("Enter a name: ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Initials: ");
+
+    
+    printf("%c.", name[0]);
+
+    
+    for (i = 0; name[i] != '\0'; i++) {
+        if (name[i] == ' ' && name[i+1] != '\0')
+            printf("%c.", name[i+1]);
+    }
+
+    return 0;
+}
