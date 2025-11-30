dffc1c119e9888e70f52c2d7ca6900300d93b333
aanyaa259 <aanyaverma259@gmail.com>
2025-11-30 11:52:30 +0530

    corrections_18

--- day18/day18_2.c
+++ day18/day18_2.c
@@ -1,25 +1,33 @@
+/* Q36: Write a program to find the HCF (GCD) of two numbers.
+
+Sample Test Cases:
+Input 1:
+12 18
+Output 1:
+6
+
+Input 2:
+7 9
+Output 2:
+1
+
+*/
 #include <stdio.h>
 
 int main() {
-    int a, b, q, r;
+    int a, b;
 
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    int x = a, y = b;  
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
 
-    printf("\nEuclidean steps to find HCF of %d and %d:\n", x, y);
-
-while (b != 0) {
-q = a / b;     // quotient
-r = a % b;
-printf("%d = %d * %d + %d\n", a, b, q, r);
-a = b;
-b = r;
+    printf("HCF = %d\n", a);
+    return 0;
 }
 
-    printf("\nHCF = %d\n", a);
-
-return 0;
-}
 
