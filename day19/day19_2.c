0421074bc703a326f2ac6c33dcde43332c0ff816
aanyaa259 <aanyaverma259@gmail.com>
2025-12-02 20:51:33 +0530

    cmmitfinal

--- day19/day19_2.c
+++ day19/day19_2.c
@@ -13,3 +13,22 @@ Output 2:
 27
 
 */
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        digit = num % 10;   // extract last digit
+        sum += digit;       // add digit to sum
+        num /= 10;          // remove last digit
+    }
+
+    printf("Sum of digits = %d\n", sum);
+
+    return 0;
+}
+
--- day31/day31_2.c
+++ day31/day31_2.c
@@ -1,3 +1,13 @@
+/* Q62: Reverse an array without taking extra space.
+
+Sample Test Cases:
+Input 1:
+4
+1 2 3 4
+Output 1:
+4 3 2 1
+
+*/
 #include <stdio.h>
 int main() {
     int n, i, key, found = -1;
